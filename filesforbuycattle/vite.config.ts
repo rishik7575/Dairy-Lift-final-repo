ea63845f521e3ff2805088c7bde2b080c8eed2fd
rishik <22wj8a05b8@gniindia.org>
2025-07-02 20:22:26 +0530

    dairyvest to dairy lift

--- filesforbuycattle/vite.config.ts
+++ filesforbuycattle/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: './', // Use relative paths
   server: {
     host: "::",
@@ -12,12 +10,11 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // Add other plugins here if needed
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "./src",
     },
   },
-}));
+});
